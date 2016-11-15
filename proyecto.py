89fccdbe43be2f5443f1025cd0aa1ee5275d2f9a
Julio Sepulveda Sepulveda <sepulveda.julioe@gmail.com>
2016-11-15 08:17:29 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,9 +1,9 @@
 def genera():
-  l=[1,2.3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
+  L=[1,2.3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]
   mi_lista=[]
      while len(mi_lista)<6:
-       numero=random.choice(l)
-      if not (numero) in l:
+       numero=random.choice(L)
+      if not (numero) in L:
         mi_lista.append(numero)
       return mi_lista
  
